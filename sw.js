a2f5eedff2634fc2e6e093517d2177119f63d166
HARI PRASANTH.S <114271109+hariprasanth209@users.noreply.github.com>
2025-05-05 09:42:27 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        tag: data.tag,
+        requireInteraction: true
+    };
+    
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
